c095b4cb982c2fa72f9496851da597575335d4a0
clear <joegung77@yahoo.com>
2022-07-14 17:43:47 -0400

    first commit

---
+++ src/test/java/com/jgung/chinesezodiacfortunebackend/ChineseZodiacFortuneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgung.chinesezodiacfortunebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChineseZodiacFortuneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
