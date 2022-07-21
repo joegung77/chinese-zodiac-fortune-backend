dbef620b48385d7567ebd389f414eaa04a266382
clear <joegung77@yahoo.com>
2022-07-21 17:12:12 -0400

    added FortuneResponseObject

---
+++ src/main/java/com/jgung/chinesezodiacfortunebackend/service/FortuneResponseObject.java
@@ -0,0 +1,17 @@
+package com.jgung.chinesezodiacfortunebackend.service;
+
+import java.util.Date;
+
+public class FortuneResponseObject {
+
+    private Zodiac zodiac;
+
+    public FortuneResponseObject() {
+
+    }
+
+    public FortuneResponseObject(Date date) {
+
+    }
+
+}
